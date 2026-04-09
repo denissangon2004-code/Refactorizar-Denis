7e4bb74f7092e6fccb072c209970a3e1d824ce7d
denissangon2004-code <denissangon2004@gmail.com>
2026-04-09 10:27:30 +0000

    Añadiendo ExtraerVariable

---
+++ Extraer_Variable.java
@@ -0,0 +1,19 @@
+public class Extraer_Variable{
+    // void renderBanner() {
+    // if ((platform.toUpperCase().indexOf("MAC") > -1) &&
+    //    (browser.toUpperCase().indexOf("IE") > -1) &&
+    // wasInitialized() && resize > 0 )
+    //   {
+    // do something
+    //   }
+    // }
+    void renderBanner() {
+        final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;
+        final boolean isIE = browser.toUpperCase().indexOf("IE") > -1;
+        final boolean wasResized = resize > 0;
+
+        if (isMacOs && isIE && wasInitialized() && wasResized) {
+            // do something
+        }
+    }
+}
